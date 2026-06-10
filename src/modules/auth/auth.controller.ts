2115960020643bd81811eea809a3ee03dbeac207
mehadishisir <mehadishisir@gmail.com>
2026-06-11 04:54:55 +0600

    add login controller and route, auth module complete

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -9,7 +9,18 @@ const signup = async (req: Request, res: Response) => {
         message: "User registered successfully",
         data: result
     })
-}
+};
+
+const login = async (req: Request, res: Response) => {
+    const result = await authService.loginService(req.body);
+    res.status(200).json({
+        success: true,
+        message: "User logged in successfully",
+        data: result
+    })
+};
+
 export const authController = {
-    signup
+    signup,
+    login
 } 
